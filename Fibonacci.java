f5b839690f224621827d2cae4729eac57d448e97
Neiva2001 <32169061+Neiva2001@users.noreply.github.com>
2018-04-19 15:12:03 +0100

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+
+package fibonacci;
+public class Fibonacci {
+
+    public static void main(String[] args) {
+     
+        int num1 = 1, num2 = 0;
+        
+        System.out.println(num2);
+        System.out.println(num1);
+        
+        for(int i = 0; i < 100000000; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
+        }
+    }
+    
+}
+    
+    
+
